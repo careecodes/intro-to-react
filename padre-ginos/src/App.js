f09fe5608994bcacfd12f6e5d7df8a39ec76d4f5
careecodes <caree@caree.codes>
2025-04-30 12:06:15 -0500

    prettier tooling setup

--- padre-ginos/src/App.js
+++ padre-ginos/src/App.js
@@ -1,31 +1,27 @@
 const Pizza = (stuff) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, stuff.name),
-        React.createElement("p", {}, stuff.description)
-    ])
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, stuff.name),
+    React.createElement("p", {}, stuff.description),
+  ]);
+};
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Padre Gino's"), 
-            React.createElement(Pizza, {
-                name: "The Dakota",
-                description: "Mozzarella, Pepperoni, and M&Ms."
-            }),
-            React.createElement(Pizza, {
-                name: "The Lucas",
-                description: "No toppings. Just the bread part."
-            }),
-            React.createElement(Pizza, {
-                name: "The Dad",
-                description: "Supeme Pizza, room temperature."
-            })
-        ]
-    )
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, {
+      name: "The Dakota",
+      description: "Mozzarella, Pepperoni, and M&Ms.",
+    }),
+    React.createElement(Pizza, {
+      name: "The Lucas",
+      description: "No toppings. Just the bread part.",
+    }),
+    React.createElement(Pizza, {
+      name: "The Dad",
+      description: "Supeme Pizza, room temperature.",
+    }),
+  ]);
+};
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
