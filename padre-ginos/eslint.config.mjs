02fb44d0ea615bc403f72bdf2b2e7a32f486dcc0
careecodes <caree@caree.codes>
2025-04-30 13:24:39 -0500

    JSX setup continued

--- padre-ginos/eslint.config.mjs
+++ padre-ginos/eslint.config.mjs
@@ -1,13 +1,27 @@
 import js from "@eslint/js";
 import globals from "globals";
 import prettier from "eslint-config-prettier";
+import reactPlugin from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   js.configs.recommended,
   {
-    files: ["**/*.js"],
+    ...reactPlugin.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  reactPlugin.configs.flat["jsx-runtime"],
+  {
+    files: ["**/*.js, **/*.jsx"],
     languageOptions: {
+      rules: {
+        "react/no-unescaped-entities": "off",
+        "react/prop-types": "off",
+      },
       globals: { ...globals.browser, ...globals.node },
       parserOptions: {
         ecmaFeatures: {
