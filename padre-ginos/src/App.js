678bb447ac4a365e3884f0cb7478b9767f513798
careecodes <caree@caree.codes>
2025-04-30 11:22:34 -0500

    vanilla components demo

--- padre-ginos/src/App.js
+++ padre-ginos/src/App.js
@@ -1,8 +1,29 @@
+const Pizza = (stuff) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, stuff.name),
+        React.createElement("p", {}, stuff.description)
+    ])
+}
+
 const App = () => {
     return React.createElement(
         "div",
         {},
-        React.createElement("h1", {}, "Padre Gino's")
+        [
+            React.createElement("h1", {}, "Padre Gino's"), 
+            React.createElement(Pizza, {
+                name: "The Dakota",
+                description: "Mozzarella, Pepperoni, and M&Ms."
+            }),
+            React.createElement(Pizza, {
+                name: "The Lucas",
+                description: "No toppings. Just the bread part."
+            }),
+            React.createElement(Pizza, {
+                name: "The Dad",
+                description: "Supeme Pizza, room temperature."
+            })
+        ]
     )
 }
 
